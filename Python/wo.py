08f18ab5e3eba5d675ce9365d8dd4e2afcdea832
studynotlimit <lillard20212021@163.com>
2022-10-12 00:21:58 +0800

    Update wo.py

--- Python/wo.py
+++ Python/wo.py
@@ -19,4 +19,27 @@ for course_num in range(1,6):
                 ran[i] = str(ran[i])
                 value[i][0] =ran[i].zfill(9)
                 # print(value[i][0])
-            
+                # 生成性别
+                for i in range(90):
+                    sex = random.randint(0, 1)
+                    if sex == 1:
+                        value[i][1] = '男'
+                    else:
+                        value[i][1] = '女'
+                # 生成年龄
+                for i in range(90):
+                    age = random.randint(20, 23)
+                    value[i][2] = age
+                # 生成大概率缺课同学的个数
+                large_num = random.randint(5, 8)
+                # 统计个数
+                count = 0
+                # 生成绩点,并确保绩点的值正确
+                grade = np.random.normal(2.7, 0.7, 90)
+                for i in range(90):
+                    while grade[i] > 4 or grade[i] < 0:
+                        grade[i] = np.random.normal(2.7, 0.7, 1)
+                grade.sort()
+                for i in range(90):
+                    value[i][3] = grade[i]
+
